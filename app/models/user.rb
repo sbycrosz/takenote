0c19d44f0606cc946b70f57121b1774de380cf2a
sambya <sambya@icehousecorp.com>
2014-01-28 20:29:57 +0700

    Added create_guest_account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessor :old_password
 
-  has_secure_password
+  has_secure_password validations: false
 
-  validates_presence_of :name, :email
-  validates_uniqueness_of :email
-  validate :validates_old_password, on: :update
+  validates_presence_of :password, unless: :guest?
+  validates_presence_of :password_confirmation, if: :password_present?
+  validates_confirmation_of :password, if: :password_present?
+
+  validates_presence_of :name, :email, unless: :guest?
+  validates_uniqueness_of :email, unless: :guest?
+  validate :validates_old_password, on: :update, unless: :guest?
 
   has_many :notes
   has_many :tags
@@ -15,5 +19,13 @@ class User < ActiveRecord::Base
 
   def validates_old_password
     errors.add(:old_password, "is incorrect") if password.present? && !authenticate(old_password)
+  end
+
+  def self.create_guest_account
+    create!(guest: true)
+  end
+
+  def password_present?
+    password.present?
   end
 end
